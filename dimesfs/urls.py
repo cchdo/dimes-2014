90cdd78894fd1241b72cfb0c1668604721bd10b2
Andrew Barna <abarna@gmail.com>
2014-10-01 13:58:32 -0700

    have file browser working

--- dimesfs/urls.py
+++ dimesfs/urls.py
@@ -4,5 +4,6 @@ from dimesfs import views
 
 urlpatterns = patterns('',
         url(r'fslist$', views.fslist, name='fslist'),
+        url(r'dirflist$', views.dirflist, name='dirflist'),
         url(r'^$', views.index, name='index'),
         )
--- dimesfs/views.py
+++ dimesfs/views.py
@@ -1,16 +1,19 @@
 import json
 from collections import defaultdict
+import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from tagstore.client import TagStoreClient, Query
 
+ts_api_endpoint = "http://umi.local:5000/api/v1"
 
 def fslist(request):
     Tree = lambda: defaultdict(Tree) # did you mean recursion?
     fs_tree = Tree()
-    tsc = TagStoreClient("http://umi.local:5000/api/v1")
+    tsc = TagStoreClient(ts_api_endpoint)
     fs_tags = tsc.query_tags(["tag", "like", "dimes_directory:%"])
     fs_taglist = [t for t in fs_tags]
     fs_pathlist = [t.tag.split(":")[1][1:].split("/") for t in fs_taglist]
@@ -22,9 +25,16 @@ def fslist(request):
     del fs_tree[""]
     return HttpResponse(json.dumps(fs_tree), content_type="application/json")
 
+@csrf_exempt
 def dirflist(request):
     fslist=[]
-    return HttpResponse(json.dumps(flist), content_type="application/json")
+    tsc = TagStoreClient(ts_api_endpoint)
+    if request.method == "POST":
+        #probably won't work on a non *nix OS
+        tag = "dimes_directory:" + os.path.join("/", *json.loads(request.body))
+        files = [d for d in tsc.query_data(Query.tags_any("eq", tag))]
+        fslist = [f.fname for f in files]
+    return HttpResponse(json.dumps(fslist), content_type="application/json")
 
 def index(request):
     return render(request, "dimesfs/index.html")
