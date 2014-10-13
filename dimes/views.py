39ff3bf0d7e5a1ce0c9ab75fed2bdb7390c8ee98
Matt Shen <myeeshen@gmail.com>
2014-10-13 16:41:33 -0700

    programmatically generate data page and add bodc links

--- dimes/views.py
+++ dimes/views.py
@@ -16,7 +16,28 @@ def publications(request):
         context_instance=RequestContext(request))
 
 def data(request):
-    return render_to_response('data.html', {},
+    cruises0 = [
+        dict(id='US1', ship='R/V Revelle', dates='2009'),
+        dict(id='UK1', ship='James Cook', dates='2009',
+             link='http://www.bodc.ac.uk/projects/international/dimes/data_inventories/cruise/jc041/'),
+        dict(id='US2', ship='R/V Thompson', dates='2010'),
+        dict(id='UK2', ship='RRS James Clark Ross', dates='2010-2011',
+             link='http://www.bodc.ac.uk/projects/international/dimes/data_inventories/cruise/jc054/'),
+        dict(id='US3'),
+        dict(id='UK2.5', ship='RRS James Clark Ross',
+             link='http://www.bodc.ac.uk/projects/international/dimes/data_inventories/cruise/jr20110409/'),
+    ]
+
+    cruises1 = [
+        dict(id='UK3', ship='RRS James Cook', dates='2011-2012',
+             link='http://www.bodc.ac.uk/projects/international/dimes/data_inventories/cruise/jc069/'),
+        dict(id='UK4', ship='RRS James Clark Ross', dates='2013'),
+        dict(id='US5'),
+        dict(id='UK5'),
+        dict(id='UK2014', ship='RRS James Cook'),
+    ]
+    return render_to_response('data.html', {"cruises0": cruises0, "cruises1":
+                                            cruises1},
         context_instance=RequestContext(request))
 
 def fieldwork(request):
